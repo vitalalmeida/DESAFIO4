3e91017c50af368383e67d15d0d2a47feb2b992e
vitalalmeida <68396349+vitalalmeida@users.noreply.github.com>
2025-05-22 13:21:09 -0300

    Delete projeto_integrado directory

--- projeto_integrado/const menuButton = document.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById('menu-button');
-const mobileMenu = document.getElementById('mobile-menu');
-
-menuButton.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-});
